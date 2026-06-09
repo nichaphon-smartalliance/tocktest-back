9e885b3d796f230341f9b16245fe86b72280f779
develyst1 <develyst1@hotmail.com>
2026-06-09 18:52:04 +0700

    feat: update environment configuration and enhance logging for database and frontend URL

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const url = process.env.FRONTEND_URL || 'http://localhost:3012'
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:4003',
+    origin: url,
     credentials: true,
   });
 
@@ -27,7 +27,9 @@ async function bootstrap() {
 
   const port = process.env.PORT || 4004;
   await app.listen(port);
-  console.log(`🚀 TockTest Backend running on http://localhost:${port}`);
+  console.log(`🚀 TockTest Backend running on ${url}`);
+  console.log(`🚀 TockTest Backend db on '${process.env.DB_HOST}:${process.env.DB_PORT}:'${process.env.DB_NAME}'`);
+  console.log(`🚀 TockTest Backend password on '${process.env.DB_PASSWORD}'`);
 }
 
 bootstrap();
