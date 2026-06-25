83d6c54b972a420d484ce64eca646efd22d7719d
pp007 <6722771373@g.siit.tu.ac.th.com>
2026-06-25 15:02:36 +0700

    feat(docs): wire UsersModule into DocsModule for email lookup

--- src/modules/docs/docs.module.ts
+++ src/modules/docs/docs.module.ts
@@ -6,12 +6,14 @@ import { DocsService } from './docs.service';
 import { RepositoriesModule } from '../repositories/repositories.module';
 import { RepoSettings } from '../settings/entities/repo-settings.entity';
 import { GithubTokensModule } from '../github-tokens/github-tokens.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ProjectDoc, RepoSettings]),
     GithubTokensModule,
     RepositoriesModule,
+    UsersModule,
   ],
   controllers: [DocsController],
   providers: [DocsService],
